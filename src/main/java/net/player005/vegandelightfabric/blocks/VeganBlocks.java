2da8e29994a7edc4dd112eeaa392762a4f0fda1f
player005 <me@player005.net>
2024-09-16 22:08:42 +0200

    improve wild soybean & make potted wild soybean work

--- src/main/java/net/player005/vegandelightfabric/blocks/VeganBlocks.java
+++ src/main/java/net/player005/vegandelightfabric/blocks/VeganBlocks.java
@@ -3,13 +3,16 @@ package net.player005.vegandelightfabric.blocks;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.player005.vegandelightfabric.VeganDelight;
 import org.jetbrains.annotations.NotNull;
+import vectorwing.farmersdelight.common.block.WildCropBlock;
 
 public class VeganBlocks {
 
@@ -17,15 +20,10 @@ public class VeganBlocks {
             register(new SoybeanCropBlock(BlockBehaviour.Properties.copy(Blocks.WHEAT)), "soybean_crop", false);
 
     public static final Block WILD_SOYBEAN =
-            register(new Block(BlockBehaviour.Properties.copy(Blocks.ALLIUM).noCollission().noOcclusion()), "wild_soybean", true);
-
-//    public static final Block WILD_SOYBEAN =
-//            register(new BushBlock(//MobEffects.DAMAGE_BOOST, 8,
-//                    Block.Properties.copy(Blocks.TALL_GRASS)), "wild_soybean", true
-//            );
+            register(new WildCropBlock(MobEffects.DAMAGE_BOOST, 12, BlockBehaviour.Properties.copy(Blocks.ALLIUM)), "wild_soybean", true);
 
     public static final Block POTTED_WILD_SOYBEAN =
-            register(new Block(BlockBehaviour.Properties.copy(Blocks.POTTED_ALLIUM).noOcclusion()), "potted_wild_soybean", false);
+            register(new FlowerPotBlock(WILD_SOYBEAN, BlockBehaviour.Properties.copy(Blocks.POTTED_ALLIUM)), "potted_wild_soybean", false);
 
     public static final Block SOYBEAN_BAG = register(new Block(BlockBehaviour.Properties.copy(Blocks.WHITE_WOOL)), "soybean_bag", true);
 
