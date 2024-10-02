3034561b7e7b9394624630f0ee7792dc0a34c014
player005 <me@player005.net>
2024-10-02 17:33:22 +0200

    add farmer villager trade (soybeans -> emerald)

--- src/main/java/net/player005/vegandelightfabric/VeganDelight.java
+++ src/main/java/net/player005/vegandelightfabric/VeganDelight.java
@@ -1,6 +1,11 @@
 package net.player005.vegandelightfabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.player005.vegandelightfabric.blocks.VeganBlocks;
 
@@ -13,7 +18,19 @@ public class VeganDelight implements ModInitializer {
         VeganFluids.initialise();
         VeganBlocks.initialise();
 
+        registerTrades();
+
+
         ComposterBlock.COMPOSTABLES.put(VeganItems.SOYBEAN, 0.45f);
         ComposterBlock.COMPOSTABLES.put(VeganBlocks.WILD_SOYBEAN, 0.65f);
+    }
+
+    private void registerTrades() {
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1,
+                (factories) -> factories.add((trader, random) -> new MerchantOffer(
+                        new ItemStack(VeganItems.SOYBEAN, random.nextIntBetweenInclusive(16, 24)),
+                        new ItemStack(Items.EMERALD, 1),
+                        12, 5, 0.05f
+                )));
     }
 }
