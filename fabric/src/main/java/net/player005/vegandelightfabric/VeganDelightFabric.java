e0fdbe9b0123749f3598ef0f8644bc60ddd44f65
player005 <me@player005.net>
2024-10-31 11:42:00 +0100

    fix fluid loading on fabric servers

--- fabric/src/main/java/net/player005/vegandelightfabric/VeganDelightFabric.java
+++ fabric/src/main/java/net/player005/vegandelightfabric/VeganDelightFabric.java
@@ -1,14 +1,12 @@
 package net.player005.vegandelightfabric;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags;
-import net.minecraft.client.renderer.RenderType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
@@ -20,6 +18,7 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.material.FlowingFluid;
+import net.player005.vegandelightfabric.client.VeganDelightClient;
 import net.player005.vegandelightfabric.fluids.FluidProperties;
 import vectorwing.farmersdelight.common.registry.ModBiomeModifiers;
 
@@ -41,6 +40,7 @@ public class VeganDelightFabric implements ModInitializer {
 
         @Override
         public FlowingFluid registerFluids(String name, FluidProperties properties) {
+            // we need to use some kind of references or java will complain
             final var flowingRef = new AtomicReference<FlowingFluid>();
             final var stillRef = new AtomicReference<FlowingFluid>();
 
@@ -58,19 +58,8 @@ public class VeganDelightFabric implements ModInitializer {
             );
             stillRef.set(still);
 
-            try {
-                // Check if we're on the client
-                Class.forName("net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry");
-
-                FluidRenderHandlerRegistry.INSTANCE.register(
-                        still, flowing, new SimpleFluidRenderHandler(
-                                ResourceLocation.fromNamespaceAndPath(VeganDelightMod.modID, "block/" + name + "_still"),
-                                ResourceLocation.fromNamespaceAndPath(VeganDelightMod.modID, "block/" + name + "_flowing")
-                        )
-                );
-
-                BlockRenderLayerMap.INSTANCE.putFluids(RenderType.translucent(), flowing, still);
-            } catch (ClassNotFoundException ignored) {
+            if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+                VeganDelightClient.registerFluidRenderers(name, still, flowing);
             }
 
             return flowing;
--- fabric/src/main/java/net/player005/vegandelightfabric/client/VeganDelightClient.java
+++ fabric/src/main/java/net/player005/vegandelightfabric/client/VeganDelightClient.java
@@ -2,10 +2,26 @@ package net.player005.vegandelightfabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.player005.vegandelightfabric.SimpleFlowableFluid;
+import net.player005.vegandelightfabric.VeganDelightMod;
 import net.player005.vegandelightfabric.blocks.VeganBlocks;
 
 public class VeganDelightClient implements ClientModInitializer {
+
+    public static void registerFluidRenderers(String name, SimpleFlowableFluid.Still still, SimpleFlowableFluid.Flowing flowing) {
+        FluidRenderHandlerRegistry.INSTANCE.register(
+                still, flowing, new SimpleFluidRenderHandler(
+                        ResourceLocation.fromNamespaceAndPath(VeganDelightMod.modID, "block/" + name + "_still"),
+                        ResourceLocation.fromNamespaceAndPath(VeganDelightMod.modID, "block/" + name + "_flowing")
+                )
+        );
+
+        BlockRenderLayerMap.INSTANCE.putFluids(RenderType.translucent(), flowing, still);
+    }
 
     @Override
     public void onInitializeClient() {
@@ -14,30 +30,5 @@ public class VeganDelightClient implements ClientModInitializer {
                 RenderType.cutout(),
                 VeganBlocks.WILD_SOYBEAN, VeganBlocks.SOYBEAN_CROP, VeganBlocks.POTTED_WILD_SOYBEAN
         );
-
-//        BlockRenderLayerMap.INSTANCE.putFluids(
-//                RenderType.translucent(),
-//                VeganFluids.SOYMILK, VeganFluids.SOYMILK,
-//                VeganFluids.APPLESAUCE, VeganFluids.APPLESAUCE
-//        );
-
-//        FluidRenderHandlerRegistry.INSTANCE.register(
-//                VeganFluids.APPLESAUCE,
-//                VeganFluids.APPLESAUCE,
-//                new SimpleFluidRenderHandler(
-//                        ResourceLocation.parse("vegandelight:block/applesauce_still"),
-//                        ResourceLocation.parse("vegandelight:block/applesauce_flow")
-//                )
-//        );
-//
-//        FluidRenderHandlerRegistry.INSTANCE.register(
-//                VeganFluids.SOYMILK,
-//                VeganFluids.SOYMILK,
-//                new SimpleFluidRenderHandler(
-//                        ResourceLocation.parse("vegandelight:block/milky_still"),
-//                        ResourceLocation.parse("vegandelight:block/milky_flow")
-//                )
-//        );
-
     }
 }
